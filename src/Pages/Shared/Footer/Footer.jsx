f95365c39d98a2528fbccac69106525cedf0afe7
marsrian <marsrian40@gmail.com>
2023-05-27 01:41:25 +0600

    doc house started

---
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <h4>
+                <p>©copyright All right reserved-Mars Rian</p>
+            </h4>
+        </div>
+    );
+};
+
+export default Footer;
